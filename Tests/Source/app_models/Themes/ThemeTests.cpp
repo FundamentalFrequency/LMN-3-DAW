c114831a68293f40e5dac13d644b70db74c7826e
Stone Preston <stonepreston@protonmail.com>
2020-07-17 14:58:46 -0500

    finish theme tests

--- Tests/Source/app_models/Themes/ThemeTests.cpp
+++ Tests/Source/app_models/Themes/ThemeTests.cpp
@@ -12,19 +12,75 @@ namespace AppModelsTests
 
         void runTest() override {
 
+            app_models::ThemeReader tm;
+            juce::ValueTree themesValueTree = tm.getThemes();
+            juce::ValueTree gruvboxTree = themesValueTree.getChildWithProperty(app_models::IDs::name, "Gruvbox");
+
+            app_models::Theme gruvboxTheme(gruvboxTree);
+
             beginTest("getName");
             {
 
-
-                app_models::ThemeReader tm;
-                juce::ValueTree themesValueTree = tm.getThemes();
-                juce::ValueTree gruvboxTree = themesValueTree.getChildWithProperty(app_models::IDs::name, "Gruvbox");
-
-                app_models::Theme gruvboxTheme(gruvboxTree);
-                expectEquals(gruvboxTheme.getName(), juce::String("Gruvbox"), "getName() returns incorrect name");
-
+                expectEquals(gruvboxTheme.getName(), gruvboxTree.getProperty(app_models::IDs::name).toString(), "getName() returns incorrect name");
 
             }
+
+            beginTest("getBackgroundColour");
+            {
+
+                expectEquals(gruvboxTheme.getBackgroundColour().toString(), gruvboxTree.getProperty(app_models::IDs::backgroundColour).toString(), "returns incorrect colour");
+
+            }
+
+            beginTest("getTextColour");
+            {
+
+                expectEquals(gruvboxTheme.getTextColour().toString(), gruvboxTree.getProperty(app_models::IDs::textColour).toString(), "returns incorrect colour");
+
+            }
+
+            beginTest("getColour1");
+            {
+
+                expectEquals(gruvboxTheme.getColour1().toString(), gruvboxTree.getProperty(app_models::IDs::colour1).toString(), "returns incorrect colour");
+
+            }
+
+            beginTest("getColour2");
+            {
+
+                expectEquals(gruvboxTheme.getColour2().toString(), gruvboxTree.getProperty(app_models::IDs::colour2).toString(), "returns incorrect colour");
+
+            }
+
+            beginTest("getColour3");
+            {
+
+                expectEquals(gruvboxTheme.getColour3().toString(), gruvboxTree.getProperty(app_models::IDs::colour3).toString(), "returns incorrect colour");
+
+            }
+
+            beginTest("getColour4");
+            {
+
+                expectEquals(gruvboxTheme.getColour4().toString(), gruvboxTree.getProperty(app_models::IDs::colour4).toString(), "returns incorrect colour");
+
+            }
+
+            beginTest("getDarkColour1");
+            {
+
+                expectEquals(gruvboxTheme.getDarColour1().toString(), gruvboxTree.getProperty(app_models::IDs::darkColour1).toString(), "returns incorrect colour");
+
+            }
+
+            beginTest("getDisabledBackgroundColour");
+            {
+
+                expectEquals(gruvboxTheme.getDisabledBackgroundColour().toString(), gruvboxTree.getProperty(app_models::IDs::disabledBackgroundColour).toString(), "returns incorrect colour");
+
+            }
+
 
 
         }
