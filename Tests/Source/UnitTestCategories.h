4297e286cdd662c0077e31a91caaeeda362507fd
Stone Preston <stonepreston@protonmail.com>
2020-08-17 17:02:22 -0500

    remove juce unit tests

--- Tests/Source/UnitTestCategories.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <juce_core/juce_core.h>
-
-namespace UnitTestCategories {
-
-    static const juce::String app_models {"AppModels"};
-    static const juce::String app_view_models {"AppViewModels"};
-}
