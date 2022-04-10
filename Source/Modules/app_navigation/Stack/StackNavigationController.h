1f0689598127b1ad38a9f6830e1a69d2d7618cb3
Stone Preston <stonepreston@protonmail.com>
2022-04-10 17:39:26 -0500

    Add attribution in stack navigation controller class

--- Source/Modules/app_navigation/Stack/StackNavigationController.h
+++ Source/Modules/app_navigation/Stack/StackNavigationController.h
@@ -1,14 +1,12 @@
+// This class is based on a very slimmed down version from the following demo:
+// https://github.com/adamski/StackDemo
+// I took some of the ideas from that demo and updated them for modern JUCE
 #pragma once
 
-namespace app_navigation
-{
-
+namespace app_navigation {
     class StackNavigationController
-        : public juce::Component
-    {
-
+    : public juce::Component {
     public:
-
         explicit StackNavigationController(juce::Component *rootComponent);
         ~StackNavigationController();
 
@@ -21,24 +19,17 @@ namespace app_navigation
         int size();
         juce::Component* getTopComponent();
 
-        class Listener
-        {
+        class Listener {
         public:
             virtual ~Listener() {}
-
             virtual void stackComponentContentChanged (StackNavigationController* stack) = 0;
-
         };
 
         void addListener (Listener* listener);
         void removeListener (Listener* listener);
-
     private:
-
         juce::Array<juce::WeakReference<juce::Component>> contentComponents;
         juce::ListenerList<StackNavigationController::Listener> listeners;
-
-
     };
 }
 
